87f59d428de50d477be6abb1c7d73925dc1234ea
José Castro <n3ggro@gmail.com>
2016-08-27 00:56:31 -0300

    Auto-generated commit [timestamp]

--- gulp/icons.js
+++
@@ -1,25 +0,0 @@
-'use strict';
-
-var path = require('path');
-
-module.exports = function(gulp, plugins, args, config, taskTarget, browserSync) {
-  var dirs = config.directories;
-  var dest = path.join(taskTarget);
-
-  // // Copy
-  // gulp.task('copy', function() {
-  //   return gulp.src([
-  //     path.join(dirs.source, '**/*'),
-  //     '!' + path.join(dirs.source, '{**/\_*,**/\_*/**}'),
-  //     '!' + path.join(dirs.source, '**/*.nunjucks')
-  //   ])
-  //   .pipe(plugins.changed(dest))
-  //   .pipe(gulp.dest(dest));
-  // });
-
-  // Icons
-  gulp.task('icons', function() { 
-    return gulp.src(dirs.nodeModules + '/font-awesome/fonts/**.*') 
-      .pipe(gulp.dest(dest + '/fonts')); 
-  });
-};
